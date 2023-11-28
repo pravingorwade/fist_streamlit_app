e52fb5550f5d87ebba75aee914c51e37050e7add
pravingorwade <pravingorwade@gmail.com>
2023-11-28 12:53:00 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title('My Parents New Health Diner')
 
 streamlit.header(' Breakfast Favorites')
 
-streamlit.text( 'Omega 3 & Blueberry Oatmeal')
+streamlit.text( '🫕 Omega 3 & Blueberry Oatmeal')
 streamlit.text ('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🐓Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
