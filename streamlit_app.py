94d8ea0a662b72b500a5fceff50cd63d022ee9dc
pravingorwade <pravingorwade@gmail.com>
2023-11-28 12:58:12 -0800

    Update streamlit_app.py
    Imported Pandas library to read csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+
 streamlit.title('My Parents New Health Diner')
 
 streamlit.header(' Breakfast Favorites')
@@ -8,3 +9,6 @@ streamlit.text ('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🐓Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
