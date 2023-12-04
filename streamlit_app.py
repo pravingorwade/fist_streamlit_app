ad9e0c2971efc2810b3f7d5cbec4da3c3f6c9bae
pravingorwade <pravingorwade@gmail.com>
2023-12-04 10:50:06 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import snowflake.connector
-
+import pandas
+import requests
+from urllib.error import URLError
 streamlit.title('My Parents New Health Diner')
 
 streamlit.header(' Breakfast Favorites')
@@ -10,7 +12,7 @@ streamlit.text ('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🐓Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
+# import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
@@ -20,8 +22,9 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 # streamlit.dataframe(my_fruit_list)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-import requests
+
 streamlit.header ('Fruityvice Fruit Advice!')
+# import requests
 #fruityvice_response = requests.get("http://fruityvice.com/api/fruit/watermelon")
 #fruityvice_response = requests.get("http://fruityvice.com/api/fruit/" + "kiwi")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
