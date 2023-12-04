1eac636168eaae17d55b8a3cb7e1714598b5b9cd
pravingorwade <pravingorwade@gmail.com>
2023-12-04 09:46:49 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,12 @@ fruityvice_response = requests.get("http://fruityvice.com/api/fruit/" + fruit_ch
 fruitvice_normalize = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruitvice_normalize)
 #streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor ()
+my_cur.execute("SELECT CURRENT _USER(), CURRENT_ACCOUNT (), CURRENT _REGION ()")
+my_data_row = my_cur. fetchone ()
+streamlit.text( "Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 
 
